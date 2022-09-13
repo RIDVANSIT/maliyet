4912993157f92ab8a4e493acc60e2ee58785ee40
Rıdvan Şit <99076569+RIDVANSIT@users.noreply.github.com>
2022-09-13 09:31:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ function myFunction(){
   let a =   (parseInt( document.querySelectorAll("input")[0].value))/100;
     
   let b =  ( parseInt( document.querySelectorAll("input")[1].value))/100;
-  
+
+  let c = (parseInt(document.getElementById("almf").value));
+
+  let d = (parseInt(document.getElementById("camf").value));
+
   if(document.querySelectorAll(".form-check-input")[0].checked){
-    let c = ((a+b)*5*110+a*b*550);
-    document.getElementById("sonuc").innerText= c;
+    let t = ((a+b)*4*c+a*b*d);
+    document.getElementById("sonuc").innerText= t;
    
 };
 if(document.querySelectorAll(".form-check-input")[1].checked){
@@ -18,7 +22,7 @@ if(document.querySelectorAll(".form-check-input")[2].checked){
   
  };
  if(document.querySelectorAll(".form-check-input")[3].checked){
-  let c = (((a+b)*2+b)*150/6+50);
+  let c = (((a+b)*2+b)*160/6+50);
   document.getElementById("sonuc").innerText= c;
  
 };
